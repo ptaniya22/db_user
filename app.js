8b9c62c37ba8835faccbbf15378b9e555c5fa616
Tiana <tiana.musiyachenko@gmail.com>
2023-09-29 21:11:09 +0500

    project completed

---
+++ app.js
@@ -0,0 +1,25 @@
+let userInfo = {};
+
+for (let i = 1; i <= 3; i++) {
+  console.clear();
+  id = i;
+  userInfo[id] = {};
+
+  userInfo[id].userName = prompt(`Enter name User ${i} `);
+  let tempAge = prompt('Enter age');
+  // userInfo[id].userAge = +prompt('Enter your age');
+  if (!isNaN(tempAge)) {
+    userInfo[id].userAge = tempAge;
+    // console.log(userInfo);
+
+    for (let key in userInfo) {
+      console.log(`User ${key}`);
+      console.log(` Name -  ${userInfo[key].userName}`);
+      console.log(` Age -  ${userInfo[key].userAge}`);
+    }
+  } else {
+    alert('Error! Enter again.');
+    i--;
+  }
+}
+console.log(userInfo);
